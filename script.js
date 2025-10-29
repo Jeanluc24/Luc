421a1e4e4ebede8ef817195cf45be286967fe55c
Jeanluc24 <musejnluc02@gmail.com>
2025-10-29 13:13:16 +0200

    Create js file

---
+++ script.js
@@ -0,0 +1,25 @@
+function opentab(tabname){
+        let tablinks = document.getElementsByClassName("tab-links");
+        let tabcontents = document.getElementsByClassName("tab-contents");
+
+        for(let tablink of tablinks){
+            tablink.classList.remove("active-link");
+        }
+        for(let tabcontent of tabcontents){
+            tabcontent.classList.remove("active-tab");
+        }
+
+        event.currentTarget.classList.add("active-link");
+        document.getElementById(tabname).classList.add("active-tab");
+
+
+    }
+
+var MenuItems = document.getElementById("MenuItems");
+
+    function openmenu(){
+        MenuItems.style.right = "0";
+    }
+    function closemenu(){
+        MenuItems.style.right = "-200px";
+    }
